f24f42edf174552183df445fb4d360a639061a90
byHYONS <byhyons@gmail.com>
2024-05-29 22:02:21 +0200

    [upd] created new component with the title • Fine Esercizio

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+    nome: 'AppTitle',
+    data(){
+        return{
+            title: 'La mia prima app con Vite!'
+        }
+    }
+}
+
+</script>
+
+<template>
+    <h1> {{ title }} </h1>
+</template>
+
+<style scoped>
+h1 {
+    font-size: 38px;
+    text-align: center;
+    
+}
+</style>
